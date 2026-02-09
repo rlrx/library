89695f235c992526c04df3aecceeabe7854fd082
Ryan <ryan.lee.ruixiang@gmail.com>
2026-02-09 22:43:09 +0800

    Setup class files

---
+++ Book.js
@@ -0,0 +1,10 @@
+class Book {
+	constructor(author, title, numOfPages, read) {
+		this.author = author;
+		this.title = title;
+		this.numOfPages = numOfPages;
+		this.read = read;
+	}
+}
+
+export { Book };
